f56f2029f21a8330ae21dabb5a43433330ed7bbf
Gopikrishna Sathyamurthy <sathyamurthygopikrishna@johndeere.com>
2017-02-14 12:25:59 -0600

    a simpler observer

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
-import {Observable, Observer} from 'rxjs';
+import {Observable} from 'rxjs';
 
 let numbers = [1, 5, 25];
 
 let source = Observable.from(numbers);
 
-class NumbersObserver implements Observer<number> {
-
-    next(value) {
-        console.log(`value: ${value}`);
-    }
-
-    error(error) {
-        console.log(`error: ${error}`);
-    }
-
-    complete() {
-        console.log('complete');
-    }
-}
-
-source.subscribe(new NumbersObserver());
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    error => console.log(`error: ${error}`),
+    () => console.log('complete')
+);
