d40305948afbd21cc01168ad4b23310a9e8341e9
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-15 16:15:08 -0600

    use flatmap to avoid nested subscription and to process inner observables
    using flat map here processes the inner observable that is returned by a typical map
    now, when next is called on click observer, it returns the resolved object from inner observable

--- src/http/fromEvent.ts
+++ src/http/fromEvent.ts
@@ -3,8 +3,9 @@ import {load} from './loader'
 import {subscribe} from '../observer/subcriber';
 
 const circle = document.getElementById('circle');
-const click = Observable.fromEvent(circle, 'click');
+const click = Observable.fromEvent(circle, 'click')
+    .flatMap(() => load('/api/movies.json'));
 
-const next = () => load('/api/movies.json').subscribe(data => console.log(data));
+const next = data => console.log(data);
 
 subscribe(click, next);
