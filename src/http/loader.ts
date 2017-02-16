baa7c41a3544605c76d8e7f5a6a57a3c9cccf58d
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-15 18:07:32 -0600

    make it simple

--- src/http/loader.ts
+++ src/http/loader.ts
@@ -1,10 +1,7 @@
 import {Observable} from 'rxjs';
 
-export const load = (url: string) => Observable.create((observer) => {
-    fetch(url)
-        .then(response => response.json(), observer.error)
-        .then(response => {
-            observer.next(response);
-            observer.complete();
-        });
-});
+export const load = (url: string) =>
+    Observable.fromPromise(
+        fetch(url)
+            .then(response => response.json())
+    );
