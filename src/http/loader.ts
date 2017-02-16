42fa085a9d2c2ad24c0933f75ad7d19e9a393b1e
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-15 18:01:47 -0600

    remove shim

--- src/http/loader.ts
+++ src/http/loader.ts
@@ -1,7 +1,7 @@
 import {Observable} from 'rxjs';
 
 export const load = (url: string) => Observable.create((observer) => {
-    (<any>window).fetch(url)
+    fetch(url)
         .then(response => response.json(), observer.error)
         .then(response => {
             observer.next(response);
