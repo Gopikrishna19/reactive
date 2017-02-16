9772b7e57d3a95dd52dbffdf6063ba1769c46051
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2017-02-15 18:08:02 -0600

    make it lazy

--- src/http/loader.ts
+++ src/http/loader.ts
@@ -1,7 +1,9 @@
 import {Observable} from 'rxjs';
 
 export const load = (url: string) =>
-    Observable.fromPromise(
-        fetch(url)
-            .then(response => response.json())
+    Observable.defer(
+        () => Observable.fromPromise(
+            fetch(url)
+                .then(response => response.json())
+        )
     );
